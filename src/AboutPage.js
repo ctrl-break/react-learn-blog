5c76b2f2869b777cc2af79c6c34c3f6a24d98d9a
Alexander Bashenin <ctrl-break@mail.ru>
2017-09-29 17:06:30 +0400

    add empty components

---
+++ src/AboutPage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class AboutPage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className='AboutPage'>
+        example componenet created!!
+      </div>
+    );
+  }
+
+}
---
+++ src/Navigation.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+export default class Navigation extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li><a href="#">link1</a></li>
+          <li><a href="#">link1</a></li>
+          <li><a href="#">link1</a></li>
+
+        </ul>
+      </nav>
+    );
+  }
+
+}
